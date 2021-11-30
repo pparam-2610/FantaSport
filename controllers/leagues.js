7e582b030291ce6a537e9eaba0c309d3277f5944
pparam-2610 <pparam2610@gmail.com>
2021-12-01 01:50:14 +0530

    admin side done

--- controllers/leagues.js
+++ controllers/leagues.js
@@ -3,39 +3,70 @@ const PrivateLeagues = require("../classes/leagues").privateLeagues;
 const PublicLeagues = require("../classes/leagues").publicLeagues;
 
 exports.displayLeagues = async (req, res) => {
-  if(!req.user) res.redirect("/unauthorized");
+  if (!req.user) res.redirect("/unauthorized");
   let privateLeagueData = await PrivateLeagues.getPrivateLeagues(req.user);
   let publicLeagueData = await PublicLeagues.getPublicLeagues(req.user);
   console.log(privateLeagueData, publicLeagueData);
-  res.render("leagues", {privateLeagues : privateLeagueData, publicLeagues : publicLeagueData});
+  res.render("leagues", {
+    privateLeagues: privateLeagueData,
+    publicLeagues: publicLeagueData,
+    userData: req.user,
+  });
 };
 
 exports.displayLeaguesMatches = async (req, res) => {
   console.log("League Code:", req.params.leagueCode);
-  let fixtureData = await League.getLeagueMatches({ leagueCode: req.params.leagueCode})
+  let fixtureData = await League.getLeagueMatches({
+    leagueCode: req.params.leagueCode,
+  });
   console.log("Fixture Data: ", fixtureData);
-  res.render("leagueMatch", { fixtureData : fixtureData, leagueCode : req.params.leagueCode });
+  res.render("leagueMatch", {
+    fixtureData: fixtureData,
+    leagueCode: req.params.leagueCode,
+  });
 };
 
 exports.createLeague = async (req, res) => {
   console.log("Body is: ", req.body, "User Is: ", req.user);
   console.log(process.env.ADMIN_ID === req.user._id);
-  if(req.user._id === process.env.ADMIN_ID){
-    await PublicLeagues.createPublicLeague({ leagueId : req.body.leagueId, stageId : req.body.stageId, user : req.user.userId, userId : req.user._id });
-  } else{
-    await PrivateLeagues.createPrivateLeague({ leagueId : req.body.leagueId, stageId : req.body.stageId, user : req.user.userId, userId : req.user._id })
+  let league;
+  if (req.user._id === process.env.ADMIN_ID) {
+    league = await PublicLeagues.createPublicLeague({
+      leagueId: req.body.leagueId,
+      stageId: req.body.stageId,
+      user: req.user.userId,
+      userId: req.user._id,
+    });
+  } else {
+    league = await PrivateLeagues.createPrivateLeague({
+      leagueId: req.body.leagueId,
+      stageId: req.body.stageId,
+      user: req.user.userId,
+      userId: req.user._id,
+    });
   }
-  res.send("Create League Karega on form submission");
+  // res.send("Create League Karega on form submission");
+  res.redirect(`http://localhost:8000/stages?code=${league.leagueCode}`);
 };
 
-exports.joinLeague = async (req,res) => {
-  if(req.user.userId == 'admin'){
+exports.joinLeague = async (req, res) => {
+  console.log("ggggggg");
+
+  if (req.user.userId == "admin") {
+    res.redirect(`http://localhost:8000/stages`);
     // await PrivateLeagues.joinPrivateLeague({ leagueCode : req.params.leagueCode, userId : req.user._id });
-  } else{
-    await PrivateLeagues.joinPrivateLeague({ leagueCode : req.body.leagueCode, userId : req.user._id });
+  } else {
+    let joinedLeague = await PrivateLeagues.joinPrivateLeague({
+      leagueCode: req.body.leagueCode,
+      userId: req.user._id,
+    });
+
+    res.redirect(
+      `http://localhost:8000/leagues?code=${joinedLeague.leagueCode}`
+    );
   }
-  res.send("Join League")
-}
+  // res.send("Join League");
+};
 
 exports.leagueLeaderboard = (req, res) => {
   //   res.send(`Display Leaderboard of league code ${req.params.leagueCode}`);
@@ -44,20 +75,27 @@ exports.leagueLeaderboard = (req, res) => {
 };
 
 exports.displayCreateTeam = async (req, res) => {
-  let teamDetails = await League.viewPlayers({ fixtureId : req.params.fixtureId });
+  let teamDetails = await League.viewPlayers({
+    fixtureId: req.params.fixtureId,
+  });
   teamDetails.leagueCode = req.params.leagueCode;
   teamDetails.fixtureId = req.params.fixtureId;
-  console.log("Team Details: ",teamDetails);
-  res.render("createTeam", {teamDetails : teamDetails});
+  console.log("Team Details: ", teamDetails);
+  res.render("createTeam", { teamDetails: teamDetails });
 };
 
 exports.createTeam = async (req, res) => {
   // console.log("Body: ", req.body);
   let playerData = req.body.playerData;
-  playerData.forEach((player)=> {
+  playerData.forEach((player) => {
     player.points = 0;
   });
   console.log(playerData);
-  let newTeam = await League.createTeam({ fixtureId: req.body.fixtureId, leagueCode: req.body.leagueCode, playerData: playerData, userId: req.user._id });
+  let newTeam = await League.createTeam({
+    fixtureId: req.body.fixtureId,
+    leagueCode: req.body.leagueCode,
+    playerData: playerData,
+    userId: req.user._id,
+  });
   res.send({ success: true });
 };
--- routes/index.js
+++ routes/index.js
@@ -9,30 +9,30 @@ const League = require("../models/leagues");
 const Users = require("../models/users");
 const Teams = require("../models/teams");
 
-router.get("/checkDbLeagues", async (req,res)=> {
+router.get("/checkDbLeagues", async (req, res) => {
   // const updateLeague = await League.findOne({_id : '61a4fe6e65c97fbf630ae8e0'});
   // updateLeague.users.push('61a23805202746fd7a3e8416');
   // await updateLeague.save();
   // res.send(updateLeague);
-  League.find({}, (err, leagues)=> {
-    if(err) throw err;
+  League.find({}, (err, leagues) => {
+    if (err) throw err;
     res.send(leagues);
   });
-})
+});
 
-router.get("/checkDbTeams", async (req,res)=> {
-  Teams.find({}, (err, teams)=> {
-    if(err) throw err;
+router.get("/checkDbTeams", async (req, res) => {
+  Teams.find({}, (err, teams) => {
+    if (err) throw err;
     res.send(teams);
   });
-})
+});
 
-router.get("/checkDbUsers", async (req,res)=> {
-  Users.find({}, (err, users)=> {
-    if(err) throw err;
+router.get("/checkDbUsers", async (req, res) => {
+  Users.find({}, (err, users) => {
+    if (err) throw err;
     res.send(users);
   });
-})
+});
 
 router.get("/", (req, res) => {
   // res.send('Hello Bhushan')
@@ -49,87 +49,101 @@ router.get("/profile", (req, res) => {
   //   res.render("leaderboard");
 });
 
-router.get("/stages",async (req, res) => {//All Stages (Isme se select karke create league karega banda
-  let data
+router.get("/stages", async (req, res) => {
+  //All Stages (Isme se select karke create league karega banda
+  let data;
   let today = new Date();
   let after = new Date(today);
-  after.setDate(after.getDate() + 20)
+  after.setDate(after.getDate() + 20);
   let dd = today.getDate();
-  let mm = today.getMonth()+1; 
+  let mm = today.getMonth() + 1;
   let yyyy = today.getFullYear();
   let dd1 = after.getDate();
-  let mm1 = after.getMonth()+1; 
+  let mm1 = after.getMonth() + 1;
   let yyyy1 = after.getFullYear();
-  if(dd<10) dd='0'+dd; 
-  if(mm<10) mm='0'+mm;
-  today = yyyy+'-'+mm+'-'+dd;
-  if(dd1<10) dd1='0'+dd1; 
-  if(mm1<10) mm1='0'+mm1;
-  after = yyyy1+'-'+mm1+'-'+dd1;
+  if (dd < 10) dd = "0" + dd;
+  if (mm < 10) mm = "0" + mm;
+  today = yyyy + "-" + mm + "-" + dd;
+  if (dd1 < 10) dd1 = "0" + dd1;
+  if (mm1 < 10) mm1 = "0" + mm1;
+  after = yyyy1 + "-" + mm1 + "-" + dd1;
 
   let config = {
-    method: 'get',
-    url: 'https://cricket.sportmonks.com/api/v2.0/fixtures?api_token='+ process.env.API_KEY + '&filter[starts_between]='+today+','+after,
-    headers: {}
+    method: "get",
+    url:
+      "https://cricket.sportmonks.com/api/v2.0/fixtures?api_token=" +
+      process.env.API_KEY +
+      "&filter[starts_between]=" +
+      today +
+      "," +
+      after,
+    headers: {},
   };
-  
+
   await axios(config)
-  .then(function (response) {
-    data = response.data.data;
-    // console.log(JSON.stringify(response.data.data));
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    .then(function (response) {
+      data = response.data.data;
+      // console.log(JSON.stringify(response.data.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
   let stageIdArr = [];
   let stageData = [];
-  data.forEach((fixture)=> {
-    if(!stageIdArr.includes(fixture.stage_id)){
+  data.forEach((fixture) => {
+    if (!stageIdArr.includes(fixture.stage_id)) {
       stageIdArr.push(fixture.stage_id);
     }
   });
-  for(let i=0; i<stageIdArr.length; i++){
-      let config2 = {
-        method: 'get',
-        url: 'https://cricket.sportmonks.com/api/v2.0/stages/'+ stageIdArr[i]  + '?api_token=' + process.env.API_KEY,
-        headers: {}
-      }
-      await axios(config2)
+  for (let i = 0; i < stageIdArr.length; i++) {
+    let config2 = {
+      method: "get",
+      url:
+        "https://cricket.sportmonks.com/api/v2.0/stages/" +
+        stageIdArr[i] +
+        "?api_token=" +
+        process.env.API_KEY,
+      headers: {},
+    };
+    await axios(config2)
       .then(async (response) => {
         stageData.push(response.data.data);
       })
       .catch(function (error) {
         console.log(error);
       });
-  };
-
-  
-  for(let j=0; j<stageData.length; j++){
-    let config3 = {
-      method: 'get',
-      url: 'https://cricket.sportmonks.com/api/v2.0/leagues/'+ stageData[j].league_id  + '?api_token=' + process.env.API_KEY,
-      headers: {}
-    }
-    await axios(config3)
-    .then((resp)=> {
-      stageData[j].image_path = resp.data.data.image_path;
-      if(stageData[j].league_id != 3) stageData[j].name = resp.data.data.name
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
   }
-  console.log("FINAL DATA:",stageData);
-  res.render("home",{stageData : stageData});
-  
+
+  for (let j = 0; j < stageData.length; j++) {
+    let config3 = {
+      method: "get",
+      url:
+        "https://cricket.sportmonks.com/api/v2.0/leagues/" +
+        stageData[j].league_id +
+        "?api_token=" +
+        process.env.API_KEY,
+      headers: {},
+    };
+    await axios(config3)
+      .then((resp) => {
+        stageData[j].image_path = resp.data.data.image_path;
+        if (stageData[j].league_id != 3)
+          stageData[j].name = resp.data.data.name;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  console.log("FINAL DATA:", stageData);
+  res.render("home", { stageData: stageData, userData: req.user });
 });
 
 router.get("/upcoming", (req, res) => {
   //   res.send(
   //     "Idhar upcoming matches pel sakte hai which will redirect to its respective league"
   //   );
-  res.render("upcoming");
+  res.render("upcoming", { userData: req.user });
 });
 
 module.exports = router;
